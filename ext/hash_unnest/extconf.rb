13cc3287ad2b2aa9eeb1954eecbb16cbff4cab9a
Julien Letessier <julien.letessier@gmail.com>
2019-04-17 22:11:17 +0200

    compile with optimisations on

--- ext/hash_unnest/extconf.rb
+++ ext/hash_unnest/extconf.rb
@@ -4,10 +4,10 @@ PLATFORM = `uname`.strip.upcase
 SHARED_FLAGS = "--std=c99 -Wall -Wextra -Werror"
 
 # production
-# $CFLAGS += " #{SHARED_FLAGS} -Os -g -DNDEBUG"
+$CFLAGS += " #{SHARED_FLAGS} -Os -g"
 
 # development
 # $CFLAGS += " #{SHARED_FLAGS} -O0 -g -DDEBUG"
-$CFLAGS += " #{SHARED_FLAGS} -Os -g -DNDEBUG"
+# $CFLAGS += " #{SHARED_FLAGS} -Os -g"
 
 create_makefile('hash_unnest/hash_unnest')
